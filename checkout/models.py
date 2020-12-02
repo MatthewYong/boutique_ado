361df17429e19b9513c2ea8da944a9b61eaabf1c
Matthew <kfm.yong@gmail.com>
2020-12-02 20:17:28 +0000

    Added order summary to checkout success page

--- checkout/models.py
+++ checkout/models.py
@@ -23,19 +23,6 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
-
-class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, 
-                            null=False, blank=False, 
-                            on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product,
-                            null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, 
-                            null=True, blank=True) # XS, S, M, L, XL
-    quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, 
-                            decimal_places=2, null=False, blank=False, editable=False)
-
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
@@ -47,7 +34,7 @@ class OrderLineItem(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
