be547aae5eef416342802cb38107931a612088c6
Matthew <kfm.yong@gmail.com>
2020-12-04 14:29:03 +0000

    Updated stripe key

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,3 +1,4 @@
+
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
@@ -6,7 +7,6 @@ from django.views.decorators.csrf import csrf_exempt
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
-
 
 @require_POST
 @csrf_exempt
